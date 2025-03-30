c3435f0f3275ccd78b87d1997e2bec29ff35d3db
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-03-30 12:48:23 -0300

    Exercício com widget

---
+++ atividade_pratica/flutter_application_1/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Título do Aativo'),
+        ),
+        body: Center(
+          child: Text('Olá Mundo!'),
+        ),
+      ),
+    );
+  }
+}
